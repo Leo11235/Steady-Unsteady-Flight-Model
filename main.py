75a5e2c17efb53c6c2912eee2ff52eede732cea0
Leo11235 <114216430+Leo11235@users.noreply.github.com>
2024-12-31 14:53:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,36 +3,30 @@ guide for dictionaries:
     rocket_inputs refers to the user's inputs, these will never change between each simulation
     rocket_parameters refers to the parameters that get calculated in each iteration of the simulation, only the final iteration's parameters are kept
     constants_dict contains universal values such as sea level gravity
+    overall_flight_dict contains n-long lists, where n is the amount of timesteps; these will house information for graph creation
+    burn_dict, ascent_dict respectively contain information about the rocket's flight during the burn and after the burn
 '''
 
 from variable_initialization import constants_dict, read_rocket_attributes
 import calculations
 from pyPROPEP import runPROPEP
 import rocket_ascent_model
+from graphing import graph_everything
 
 def main():
     # create dictionary of rocket inputs
     rocket_inputs = read_rocket_attributes("./input_files/Esteban's_ancalagon.json")    
-    # create a dictionary of rocket parameters, these values will change after every iteration
-    rocket_parameters = {}
 
-    # iterate over inner radius
+    # iterate over fuel inner radius to eventually reach the right apogee
+    rocket_parameters, overall_flight_dict = iterate_over_fuel_mass(rocket_inputs, constants_dict)
 
-    # TESTING: one single iteration
-    rocket_parameters["initial internal fuel radius"] = 0.039974 # random inner radius for testing
-    # step 1
-    calculations.CV2_calculations(rocket_inputs, rocket_parameters) # run CV2 with this r
-    # step 2
-    runPROPEP(rocket_inputs, rocket_parameters)
-    # step 3
-    calculations.CV3_calculations(rocket_inputs, rocket_parameters, constants_dict)
-    # step 4: CV4 calculations
-    rocket = rocket_ascent_model.Rocket(rocket_inputs, rocket_parameters)
-
-
+    
+    # DEBUGGING 
+    graph_everything(overall_flight_dict)
     print_dict(rocket_inputs)
     print_dict(rocket_parameters)
-
+    #print_dict(timesteps_dict)
+    #print_dict(ascent_dict)
 
 
     # altitude convergence (iteration)
@@ -45,6 +39,45 @@ def main():
     # parametric studies
 
     # put relevant results in a JSON file
+
+
+# ITERATION
+# inner radius determines fuel mass -> fuel mass determines rocket's performance -> the rocket reaches a certain altitude
+def iterate_over_fuel_mass(rocket_inputs, constants_dict):
+    # create a dictionary of rocket parameters, these values will change after every iteration
+    rocket_parameters = {}
+
+    # set initial guess for internal radius using local variables
+    lower_bound = 0
+    upper_bound = rocket_inputs["fuel external diameter"] / 2 # diameter -> radius
+    Ri0 = (upper_bound + lower_bound) / 2
+
+    # iteration goes here
+    i = 1 # num of iterations
+    rocket_parameters = { 'reached apogee' : 0 } # bogus value so the while loop can start
+    while(abs(rocket_parameters["reached apogee"] - rocket_inputs["target apogee"]) >= constants_dict["tolerated apogee difference"]):
+        # step 0: clear dictionaries, set lower bound
+        rocket_parameters = { "initial internal fuel radius": Ri0 }        
+        # step 1
+        calculations.CV2_calculations(rocket_inputs, rocket_parameters) # run CV2 with this r
+        # step 2
+        runPROPEP(rocket_inputs, rocket_parameters)
+        # step 3
+        calculations.CV3_calculations(rocket_inputs, rocket_parameters, constants_dict)
+        # step 4: CV4 calculations
+        rocket = rocket_ascent_model.Rocket(rocket_inputs, rocket_parameters) # create a rocket object with all the atributes previously calculated
+        burn_dict, ascent_dict = rocket.calculate_kinematics(rocket_inputs, rocket_parameters, constants_dict) # calculate burn and ascent
+        overall_flight_dict = rocket_ascent_model.merge_flight_dictionaries(burn_dict, ascent_dict) # create a dictionary with tons of information on forces, kinematics, with respect to time
+        # step 5: refine inner radius guess; adjust bounds
+        if rocket_parameters["reached apogee"] > rocket_inputs["target apogee"]:
+            lower_bound = Ri0 # if the rocket flies too high, increase inner radius (less fuel)
+        elif rocket_parameters["reached apogee"] < rocket_inputs["target apogee"]:
+            upper_bound = Ri0 # & vice versa
+        Ri0 = (upper_bound + lower_bound) / 2
+        i += 1
+    
+    return rocket_parameters, overall_flight_dict
+
 
 
 
