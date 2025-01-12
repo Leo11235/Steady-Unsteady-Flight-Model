35f3b222024e4fe5b0f5c9daba391fd8be0f4c9d
Leo11235 <114216430+Leo11235@users.noreply.github.com>
2025-01-12 13:34:57 -0500

    Update variable_initialization.py

--- backend/variable_initialization.py
+++ backend/variable_initialization.py
@@ -6,7 +6,7 @@ that is nice to use for the rest of the program
 import json
 
 # list of required inputs
-required_keys = [
+required_keys = [ # no longer checked / needed, keeping for now for testing
     "rocket name",
 
     # combustion stuff
@@ -56,32 +56,39 @@ constants_dict = {
 
     # for computation purposes
     "number of timesteps": 100, # number of timesteps in burntime (only accounts for the part of the burn where there is a thrust). Ascent timesteps typically 300-400
-    "tolerated apogee difference": 1, # m, allowed space between target and real apogee
+    "tolerated apogee difference": .01, # m, allowed space between target and real apogee
+    "smallest allowed inner fuel radius": 0.01, # m; equivalent to 1 cm
 }
 
-# main function of this class, takes input file, outputs a dictionary rocket_inputs
-# this entire function may get deleted if I can get the frontend to directly send a dictionary to this code
-def read_rocket_attributes(json_file_path):
-    rocket_inputs = {}
+# dict containing all the kinds of errors that may occur in the program
+# this gets passed to the frontend so the user can see why their inputs failed
+# errors must be formatted in this way: 
+# "errortype error": ...
+# "errortype error information": ...
+error_dict = {
+    "error": False, # if this is still set to false when it gets passed to the frontend, this entire dictionary is ignored
+    "bisection error": False, # if the rocket is unable to reach target apogee
+    "bisection error information": "",
+    "po error": False, 
+    "po error information": "blablabla",
+}
 
-    # read JSON file and populate rocket_inputs
-    try:
-        with open(json_file_path, 'r') as file:
-            rocket_inputs = json.load(file)
-    except FileNotFoundError:
-        return "Error: The specified JSON file does not exist."
-    except json.JSONDecodeError:
-        return "Error: The JSON file is not properly formatted."
-
-    # validate required keys
-    missing_keys = validate_required_parameters(rocket_inputs, required_keys)
-    if missing_keys:
-        return f"Missing required parameters: {', '.join(missing_keys)}"
+# process inputs, convert to SI
+def read_rocket_attributes(input_file):
+    rocket_inputs = input_file
+    # convert to sane (SI unit) values
+    rocket_inputs["chamber pressure"] = float(rocket_inputs["chamber pressure"]) * 6894.76 # psi to Pa
+    rocket_inputs["oxidizer mass flow rate"] = float(rocket_inputs["oxidizer mass flow rate"])
+    rocket_inputs["fuel external diameter"] = float(rocket_inputs["fuel external diameter"]) * 0.0254 # convert from in to m
+    rocket_inputs["fuel length"] = float(rocket_inputs["fuel length"]) * 0.0254 # convert from in to m
+    rocket_inputs["target apogee"] = float(rocket_inputs["target apogee"]) * 0.3048 # ft to m
+    rocket_inputs["launch site altitude"] = float(rocket_inputs["launch site altitude"]) * 0.3048 # ft to m
+    rocket_inputs["dry mass"] = float(rocket_inputs["dry mass"])
+    rocket_inputs["rocket external diameter"] = float(rocket_inputs["rocket external diameter"]) * 0.0254 # convert from in to m
+    rocket_inputs["drag coefficient"] = float(rocket_inputs["drag coefficient"])
+    rocket_inputs["launch angle"] = float(rocket_inputs["launch angle"])
+    rocket_inputs["fuel grain density"] = float(rocket_inputs["fuel grain density"])
+    rocket_inputs["regression rate scaling coefficient"] = float(rocket_inputs["regression rate scaling coefficient"])
+    rocket_inputs["regression rate exponent"] = float(rocket_inputs["regression rate exponent"])
 
     return rocket_inputs
-
-
-# make sure all required parameters are present
-def validate_required_parameters(parameters, required_keys):
-    missing_keys = [key for key in required_keys if key not in parameters]
-    return missing_keys  # Returns a list of missing keys, empty if all keys are present
