09f325998dcee191e6a964643c70823f509b0dc9
Leo11235 <leo.amorim2004@gmail.com>
2025-10-30 16:27:59 -0400

    Update unsteady_CV1.py

--- backend/unsteady_CV1.py
+++ backend/unsteady_CV1.py
@@ -1,1 +1,15 @@
 # Tank control volume: computes [n_v, n_l, T_T] for each timestep
+
+def tank_CV1(liquid_oxidizer_volume):
+    if liquid_oxidizer_volume > 0: # liquid blowdown
+        return lquid_oxidizer_blowdown()
+    elif liquid_oxidizer_volume == 0: # gaseous blowdown
+        return gaseous_oxidizer_blowdown()
+    else:
+        print("Error in CV1: liquid_oxidizer_volume neither >0 or =0")
+
+def lquid_oxidizer_blowdown():
+    return
+
+def gaseous_oxidizer_blowdown():
+    return
