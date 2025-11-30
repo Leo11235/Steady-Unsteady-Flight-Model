b106adcc921091774680b4baf9745030cf2af3a2
ryalloric <69326132+ryalloric@users.noreply.github.com>
2025-11-30 17:41:59 -0500

    updated cc_pressure values

--- validation/hotfires/hotfire_processed/get_avg_and_max.py
+++ validation/hotfires/hotfire_processed/get_avg_and_max.py
@@ -1,37 +1,51 @@
 # Reads json files of the processed data, outputs the max and average thrust values
 
-with open("HOTFIRE4.1.jsonc", 'r') as file:
-    MIN = 6630.3
-    MAX = 6662
-    time = 0
-    thrust = 0
-    largest = 0
-    total = 0
-    i = 0
-    avg = 0
-    times = []
-    thrustnums = []
-    for line in file:
-        #print(line)
-        if time == 1 and (line != "{\n") and (line != "    \"seconds\": [\n") and (line != "    \"thrust\": [\n") and (line != "    ],\n"):
-            line = line.strip(",\n")
-            times.append(float(line))
-        if thrust == 1 & (line != "    ]\n") & (line != "}"):
-            line = line.strip(",\n")
-            thrustnums.append(float(line))
-        if line == "    \"seconds\": [\n":
-            time = 1
-        if line == "    \"thrust\": [\n":
-            #print("test")
-            thrust = 1
-            time = 0
-    for num in thrustnums:
-        #total += num
-        if times[i] >= MIN and times[i] <= MAX:
-            total += num
-            if(num > largest):
-                largest = num
-        i += 1
-    avg = total / i
-    print(avg)
-    print(largest)
+arr = ["2.2", "2.3", "2.4", "2.6", "2.7", "3.1", "3.4", "3.5", "4.1"]
+
+for n in arr:
+    with open(f"validation/hotfires/hotfire_processed/HOTFIRE{n}.jsonc", 'r') as file:
+        MIN = [7383.9, 4798, 3622.4, 4322.1, 3473.5, 4697.9, 4339.0, 2205.8, 6630.3]
+        MAX = [7390, 4806, 3634, 4333, 3485, 4711, 4349, 2238, 6662]
+        time = 0
+        thrust = 0
+        largest = 0
+        total = 0
+        i = 0
+        avgdivisor = 0
+        avg = 0
+        times = []
+        thrustnums = []
+        startinterval = 0
+        endinterval = 0
+
+        for line in file:
+            #print(line)
+            if time == 1 and (line != "{\n") and (line != "    \"seconds\": [\n") and (line != "    \"cc_pressure\": [\n") and (line != "    ],\n"):
+                line = line.strip(",\n")
+                times.append(float(line))
+            if thrust == 1 & (line != "    ]\n") & (line != "}"):
+                line = line.strip(",\n")
+                thrustnums.append(float(line))
+            if line == "    \"seconds\": [\n":
+                time = 1
+            if line == "    \"cc_pressure\": [\n":
+                #print("test")
+                thrust = 1
+                time = 0
+        for num in thrustnums:
+            #total += num
+            if times[i] >= MIN[arr.index(n)] and times[i] <= MAX[arr.index(n)]:
+                #if startinterval == 0:
+                #    print(num)
+                #    startinterval = 1
+                total += num
+                if(num > largest):
+                    largest = num
+                avgdivisor += 1
+            i += 1
+            #if endinterval == 0 and times[i] >= MAX[arr.index(n)]:
+            #    print(num)
+            #    endinterval = 1
+        avg = total / avgdivisor
+        print(f"Avg cc_pressure for hotfire {n}: {avg}")
+        print(f"Max cc_pressure for hotfire {n}: {largest}")
