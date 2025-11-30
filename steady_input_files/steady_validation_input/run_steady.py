0ba16baf16984e0406e28225eded74faf05ea04b
ryalloric <69326132+ryalloric@users.noreply.github.com>
2025-11-30 17:29:54 -0500

    Updated cc_pressure values

---
+++ steady_input_files/steady_validation_input/run_steady.py
@@ -0,0 +1,17 @@
+import json
+from backend.steady_main import main, print_dict
+if __name__ == '__main__':
+    # get file and run simulation
+    # file = "./steady_validation_input_jsons/Joel's_steady_comparison_1.jsonc"
+    # data = main(file)
+    # print_dict(data["rocket parameters"])
+    file = "./steady_validation_input_jsons/Hotfire_4.1_steady.jsonc"
+    data = main(file)
+    print_dict(data)
+    # estimate 'file size' of data
+    json_str = json.dumps(data)
+    size_bytes = len(json_str.encode('utf-8'))
+    size_kb = size_bytes / 1024
+    size_mb = size_kb / 1024
+    print(f"Estimated JSON size: {size_bytes} bytes ({size_kb:.2f} KB / {size_mb:.2f} MB)")
+    
