d9f349bf12985d66456361a6837fbbae7586add4
Leo11235 <leo.amorim2004@gmail.com>
2025-11-30 22:44:58 -0500

    more steady json inputs

--- run_steady.py
+++ run_steady.py
@@ -3,13 +3,20 @@ from backend.steady_main import main, print_dict
 
 if __name__ == '__main__':
     # get file and run simulation
-    file = "./steady_input_files/Esteban's_Ancalagon.jsonc"
-    data = main(file)
+    HOTFIRE = "3.5"
+    file = f"./steady_validation_input_files/Hotfire_{HOTFIRE}_steady.jsonc"
+    rocket_inputs, rocket_parameters = main(file)
     
-    # estimate 'file size' of data
-    json_str = json.dumps(data)
-    size_bytes = len(json_str.encode('utf-8'))
-    size_kb = size_bytes / 1024
-    size_mb = size_kb / 1024
-    print(f"Estimated JSON size: {size_bytes} bytes ({size_kb:.2f} KB / {size_mb:.2f} MB)")
+    print(f"Thrust: {rocket_parameters.get("thrust", ":(")}")
+    print(f"Total impulse: {rocket_parameters.get("total impulse")}")
+    print(f"Fuel mass: {rocket_parameters.get("fuel mass")}")
+    print(f"Burntime: {rocket_parameters.get("burntime")}")
+    
+    
+    # # estimate 'file size' of data
+    # json_str = json.dumps(data)
+    # size_bytes = len(json_str.encode('utf-8'))
+    # size_kb = size_bytes / 1024
+    # size_mb = size_kb / 1024
+    # print(f"Estimated JSON size: {size_bytes} bytes ({size_kb:.2f} KB / {size_mb:.2f} MB)")
 
