bbbb9c8eef32db53de3005698669c040fdb60adb
ZacharyTrouve <62184907+ZacharyTrouve@users.noreply.github.com>
2026-01-12 18:18:34 -0500

    fixed graph name

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -1,8 +1,8 @@
 from .PROPEP.interpolate_PROPEP import pyPROPEP_interpolation_lookup
 from .steady.steady_main import steady_main
 from .unsteady.unsteady_main import unsteady_main
-from .validation import validation
+from .validation import validation_utils
 
 print("importing backend")
 
-__all__ = ["pyPROPEP_interpolation_lookup", "steady_main", "unsteady_main", "validation"]
+__all__ = ["pyPROPEP_interpolation_lookup", "steady_main", "unsteady_main", "validation_utils"]
